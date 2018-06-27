e584ae70023f62a0a8157d10a08f806f5be25e0b
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-27 21:07:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,7 @@ class Song
 
 
   def self.find_or_create_by_name(name)
-    existing = self.find_by_name(name)
-    if self.all.name.include?(name)
-      existing
+    if self.find_by_name(name) = name
     else
       self.create_by_name(name)
     end
